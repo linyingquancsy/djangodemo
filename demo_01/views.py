40c2b0bcb0ac440d482f38ab4f31f0a4fbf80f84
linyingquan <18282513893@163.com>
2020-07-14 19:23:06 +0800

    add voicd html css

--- demo_01/views.py
+++ demo_01/views.py
@@ -1,59 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import time
-import speech_recognition as sr
-from ais_cli import tuling123
+import datetime
 
-tuling_user_id = '421096'
-tuling_api_key = '1f55700b32a14243a6ea696b1423d511'
-snowboy_location = '/home/pi/voice_assistant/sb/'
-snowboy_models = ['/home/pi/voice_assistant/sb/models/snowboy.umdl']
-snowboy_config = (snowboy_location, snowboy_models)
-
-import sys
-sys.path.append(snowboy_location)
-import snowboydecoder
-sys.path.pop()
-
-r = sr.Recognizer()
-tuling = tuling123(user_id=tuling_user_id, api_key=tuling_api_key)
+context = {'texts':[]}
 
 def demo_01(request):
-    context = {
-        'texts':[
-            {
-                "speak":"#",
-                "answer":"$"
-            }
-        ]
-    }
-
-    with sr.Microphone(sample_rate=16000) as source:
-        try:
-            print("开始监听……")
-            audio = r.listen(source,
-                             phrase_time_limit=6,
-                             snowboy_configuration=snowboy_config,
-                             hot_word_callback=snowboydecoder.play_audio_file
-                             )
-
-            print("开始识别……")
-            snowboydecoder.play_audio_file(fname=snowboy_location + 'resources/dong.wav')
-            result = r.recognize_google_cn(audio, language='zh-CN')
-        except sr.UnknownValueError:
-            result = ''
-        except Exception as e:
-            print("识别错误：{0}".format(e))
-        print("识别结果：" + result)
-
-        try:
-            speech = tuling.command(result)
-            print(speech)
-        except Exception as e:
-            print("通讯失败：{0}".format(e))
-
-    addtext(context,result,speech)
-
+    print(request.method)
+    if request.method == 'POST':
+        speak = request.POST.get('speak',0)
+        answer = request.POST.get('answer',0)
+        addtext(context,speak,answer)
     return render(request, 'index.html', context=context)
 
 def addtext(context,result,speech):
