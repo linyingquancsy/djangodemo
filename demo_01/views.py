f423093bfa75a84abc49447afb8fd72d99457ca6
linyingquan <18282513893@163.com>
2020-07-14 12:42:26 +0800

    add demo_01 app

---
+++ demo_01/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def demo_01(request):
+    return HttpResponse("hello word")
+
--- django_1_1/urls.py
+++ django_1_1/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from demo_01 import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('demo_01/',views.demo_01)
 ]
